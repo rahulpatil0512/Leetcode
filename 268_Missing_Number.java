93e71b1911a48117d1c1a4d09998deab4fea32c5
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-11-29 01:03:30 +0530

    Create 268_Missing_Number.java
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
    
     
    
    Example 1:
    
    Input: nums = [3,0,1]
    Output: 2
    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
    Example 2:
    
    Input: nums = [0,1]
    Output: 2
    Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
    Example 3:
    
    Input: nums = [9,6,4,2,3,5,7,0,1]
    Output: 8
    Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.

---
+++ 268_Missing_Number.java
@@ -0,0 +1,13 @@
+// 268. Missing Number
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int total = n*(n+1)/2;
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+        }
+        return total-sum;    
+    }
+}
